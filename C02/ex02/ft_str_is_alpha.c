0328d309272f5f746f0d189d1bf886d957ef03be
Vinicius Lopes <contact.viniciuslopes@gmail.com>
2024-09-11 00:02:31 +0100

    Add files via upload

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -6,7 +6,7 @@
 /*   By: vilopes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 22:08:16 by vilopes           #+#    #+#             */
-/*   Updated: 2024/09/09 23:10:28 by vilopes          ###   ########.fr       */
+/*   Updated: 2024/09/10 23:35:54 by vilopes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*
@@ -16,8 +16,9 @@ int	ft_str_is_alpha(char *str);
 
 int	main(void)
 {
-	ft_str_is_alpha("abcdefghijkl");
-	ft_str_is_alpha("Hello Woi90rld!");
+	printf("Resultado 1:        %i\n", ft_str_is_alpha("abcdefghijkldas"));
+	printf("Resultado 2:        %i\n", ft_str_is_alpha("Hello Woi90rld!"));
+
 	return (0);
 }
 */
@@ -28,7 +29,8 @@ int	ft_str_is_alpha(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if ((str[i] >= 97 && str[i] <= 122) || (str[i] >= 65 && str[i] <= 90))
+		if ((str[i] >= 97 && str[i] <= 122)
+			|| (str[i] >= 65 && str[i] <= 90))
 			i++;
 		else
 			return (0);
--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -6,28 +6,35 @@
 /*   By: vilopes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 23:26:02 by vilopes           #+#    #+#             */
-/*   Updated: 2024/09/09 23:30:28 by vilopes          ###   ########.fr       */
+/*   Updated: 2024/09/10 23:52:09 by vilopes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
-int ft_str_is_lowercase(char *str);
+
+#include <stdio.h>
+
+int	ft_str_is_lowercase(char *str);
 
 int	main(void)
 {
-	return 0;
+	printf("Resposta 1:	%i\n", ft_str_is_lowercase("Hello, World!"));
+	printf("Resposta 2:	%i\n", ft_str_is_lowercase("helloworld"));
+	return (0);
 }
-*/
+
 int	ft_str_is_lowercase(char *str)
 {
 	int	i;
 
 	i = 0;
+	if (str[i] == '\0')
+		return (1);
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 97 && str[i] <= 122)
-			i++;
-		else
+		if (!(str[i] >= 97 && str[i] <= 122))
+		{
 			return (0);
+		}
+		i++;
 	}
 	return (1);
 }
